d999f6850576f184a336337c003441875f694c7e
christopher211 <christopher211@users.noreply.github.com>
2018-11-19 21:21:22 +0700

    update 2

---
+++ ECM_DTO/TestGithub.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECM_DTO
+{
+    public class TestGithub
+    {
+        private int testGH;
+    }
+}
