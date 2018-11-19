092ae613fa11e322128f362ec689dcada9b603b5
Vissz <hieule0207@gmail.com>
2018-11-19 21:49:48 +0700

    hieu update

---
+++ ECM_DTO/Test2DTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECM_DTO
+{
+    public class Test2DTO
+    {
+        private int test;
+
+        public int Test
+        {
+            get { return test; }
+            set { test = value; }
+        } 
+    }
+}
